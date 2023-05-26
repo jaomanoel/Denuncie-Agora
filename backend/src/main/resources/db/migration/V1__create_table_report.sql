d66630d8085c682cb7072914b77e71b491b19282
jaomanoel <belezajovaniatb@gmail.com>
2023-05-26 03:40:26 -0300

    chore(ReportMigration): add columns identity and about on migration

--- backend/src/main/resources/db/migration/V1__create_table_report.sql
+++ backend/src/main/resources/db/migration/V1__create_table_report.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS report (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    identity VARCHAR(36) NOT NULL,
+    about VARCHAR NOT NULL,
     date DATE NOT NULL,
-    state VARCHAR(255) NOT NULL,
-    city VARCHAR(255) NOT NULL,
+    state VARCHAR NOT NULL,
+    city VARCHAR NOT NULL,
     description TEXT NOT NULL
 );
