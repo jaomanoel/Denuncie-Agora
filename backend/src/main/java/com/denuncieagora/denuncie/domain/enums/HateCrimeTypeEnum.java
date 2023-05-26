66ee08fb88695aa465490b60edae5ba82f065cac
jaomanoel <belezajovaniatb@gmail.com>
2023-05-26 03:11:43 -0300

    chore(HateCrimeType): add id for HateCrimeTypeEnum

--- backend/src/main/java/com/denuncieagora/denuncie/domain/enums/HateCrimeTypeEnum.java
+++ backend/src/main/java/com/denuncieagora/denuncie/domain/enums/HateCrimeTypeEnum.java
@@ -1,9 +1,16 @@
 package com.denuncieagora.denuncie.domain.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum HateCrimeTypeEnum {
-    RACISM,
-    XENOPHOBIA,
-    HOMOPHOBIA,
-    RAPE,
-    KIDNAPPING
+    RACISM (1),
+    XENOPHOBIA (2),
+    HOMOPHOBIA (3),
+    RAPE (4),
+    KIDNAPPING (5);
+
+    private Integer id;
 }
