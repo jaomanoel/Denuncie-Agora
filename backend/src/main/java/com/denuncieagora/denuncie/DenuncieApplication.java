dd35759783a770855c56908a88e1d3a4f56a2de6
jaomanoel <belezajovaniatb@gmail.com>
2023-05-25 23:24:12 -0300

    first commit

---
+++ backend/src/main/java/com/denuncieagora/denuncie/DenuncieApplication.java
@@ -0,0 +1,13 @@
+package com.denuncieagora.denuncie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenuncieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenuncieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/denuncieagora/denuncie/DenuncieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denuncieagora.denuncie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenuncieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
