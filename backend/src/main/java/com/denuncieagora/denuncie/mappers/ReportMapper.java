d7f08e689549f0e330e44c0daf5d1d9495b16e09
jaomanoel <belezajovaniatb@gmail.com>
2023-05-26 03:39:20 -0300

    chore(ReportMapper): created conversion from Integer to Hate Crime Type Enum

--- backend/src/main/java/com/denuncieagora/denuncie/mappers/ReportMapper.java
+++ backend/src/main/java/com/denuncieagora/denuncie/mappers/ReportMapper.java
@@ -1,9 +1,13 @@
 package com.denuncieagora.denuncie.mappers;
 
 import com.denuncieagora.denuncie.domain.entities.Report;
+import com.denuncieagora.denuncie.domain.enums.HateCrimeTypeEnum;
+import com.denuncieagora.denuncie.dtos.requests.ReportRequestDTO;
 import com.denuncieagora.denuncie.dtos.responses.ReportResponseDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+
+import java.util.stream.Stream;
 
 @Mapper(componentModel = "spring")
 public interface ReportMapper {
@@ -11,4 +15,17 @@ public interface ReportMapper {
     ReportMapper INSTANCE = Mappers.getMapper(ReportMapper.class);
 
     ReportResponseDTO toResponse(Report model);
+
+    Report toModel(ReportRequestDTO request);
+
+    default HateCrimeTypeEnum toHateCrimeTypeEnum(Integer value) {
+        return Stream.of(HateCrimeTypeEnum.values())
+                .filter(type -> type.getId().equals(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("About is invalid!"));
+    }
+
+    default Integer toInteger(HateCrimeTypeEnum value) {
+        return value.getId();
+    }
 }
