6e31cdc6d85ab8c86397f107b85cf02e5124121f
jaomanoel <belezajovaniatb@gmail.com>
2023-05-26 04:08:46 -0300

    feat(ReportService): create method for delete report

--- backend/src/main/java/com/denuncieagora/denuncie/domain/services/ReportService.java
+++ backend/src/main/java/com/denuncieagora/denuncie/domain/services/ReportService.java
@@ -2,11 +2,13 @@ package com.denuncieagora.denuncie.domain.services;
 
 import com.denuncieagora.denuncie.domain.entities.Report;
 import com.denuncieagora.denuncie.domain.repositories.ReportRepository;
+import com.denuncieagora.denuncie.dtos.requests.ReportDeleteRequestDTO;
 import com.denuncieagora.denuncie.dtos.requests.ReportRequestDTO;
 import com.denuncieagora.denuncie.dtos.responses.ReportResponseDTO;
 import com.denuncieagora.denuncie.mappers.ReportMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -31,5 +33,15 @@ public class ReportService {
         repository.save(model);
 
         return mapper.toResponse(model);
+    }
+
+    @Transactional
+    public void delete(ReportDeleteRequestDTO request){
+        Report model = repository.findById(request.getId())
+                .orElseThrow(() -> new IllegalArgumentException("id is invalid"));
+
+        if(!model.getIdentity().equals(request.getIdentity())) throw new IllegalArgumentException("identity is invalid");
+
+        repository.delete(model);
     }
 }
