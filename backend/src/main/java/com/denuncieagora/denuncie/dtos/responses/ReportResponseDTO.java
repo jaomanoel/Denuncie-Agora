34a05e4b5ec3f674c4a6593dde3fe7fdd592cf1c
jaomanoel <belezajovaniatb@gmail.com>
2023-05-26 03:12:41 -0300

    chore(ReportResponseDTO): add method identity for ReportRequestDTO

--- backend/src/main/java/com/denuncieagora/denuncie/dtos/responses/ReportResponseDTO.java
+++ backend/src/main/java/com/denuncieagora/denuncie/dtos/responses/ReportResponseDTO.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 public class ReportResponseDTO {
 
     private UUID id;
+    private String identity;
     private HateCrimeTypeEnum about;
     private LocalDate date;
     private String state;
