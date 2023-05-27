ced200313fb7adefe9f07cda4965c90e1000184a
jaomanoel <belezajovaniatb@gmail.com>
2023-05-27 17:00:36 -0300

    style(index): Update import components themed

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,14 +1,42 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { FlatList, StyleSheet } from "react-native";
+import { Post } from "../../src/components/Post";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import { View } from "../../src/components/Themed";
 
 export default function TabOneScreen() {
+  const [reports, setReports] = useState<ReportProps[]>([]);
+
+  const getReports = useCallback(async () => {
+    await axios
+      .get<ReportProps[]>("http://192.168.16.139:8080/reports")
+      .then((response) => setReports(response.data))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    getReports();
+  }, [getReports]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <FlatList
+        keyExtractor={(item: ReportProps) => item.id}
+        data={reports}
+        renderItem={({ item }) => (
+          <Post
+            date={item.date}
+            description={item.description}
+            about={item.about}
+            id={item.id}
+            identity={item.identity}
+            state={item.state}
+            city={item.city}
+          />
+        )}
+      />
     </View>
   );
 }
@@ -16,16 +44,18 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    paddingRight: 20,
+    paddingLeft: 20,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
