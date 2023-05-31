68525ef80c9e2f5b7d405381b6bf44234bcf2461
jaomanoel <belezajovaniatb@gmail.com>
2023-05-31 03:00:29 -0300

    chore(LightTheme): created theme light for styled-component

---
+++ mobile/src/theme/lightTheme.ts
@@ -0,0 +1,8 @@
+import colors from "@constants/colors";
+import { Space as space } from "@constants/space";
+import {DefaultTheme} from "styled-components/native"
+
+export const lightTheme: DefaultTheme = {
+  colors: colors.light,
+  space: space,
+};
