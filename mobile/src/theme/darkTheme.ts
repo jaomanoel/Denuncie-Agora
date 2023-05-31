20904a80e35e60aae6e2243b31823c42ccc0df7a
jaomanoel <belezajovaniatb@gmail.com>
2023-05-31 03:14:48 -0300

    chore(DarkTheme): added new propertie fontSize

--- mobile/src/theme/darkTheme.ts
+++ mobile/src/theme/darkTheme.ts
@@ -1,8 +1,10 @@
 import colors from "@constants/colors";
+import { fontSizes } from "@constants/fontSizes";
 import { Space as space } from "@constants/space";
 import { DefaultTheme } from "styled-components/native";
 
 export const darkTheme: DefaultTheme = {
   colors: colors.dark,
-  space: space,
+  spaces: space,
+  fontSizes: fontSizes
 };
