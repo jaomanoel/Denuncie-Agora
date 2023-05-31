96144bf12365893ae6dde85edb31765f5339aa6a
jaomanoel <belezajovaniatb@gmail.com>
2023-05-31 03:00:03 -0300

    chore(DarkTheme): created theme dark for styled-component

---
+++ mobile/src/theme/darkTheme.ts
@@ -0,0 +1,8 @@
+import colors from "@constants/colors";
+import { Space as space } from "@constants/space";
+import { DefaultTheme } from "styled-components/native";
+
+export const darkTheme: DefaultTheme = {
+  colors: colors.dark,
+  space: space,
+};
