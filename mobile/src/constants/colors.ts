2b378a86afe72f22a0de5cb3f121fa96352d1640
jaomanoel <belezajovaniatb@gmail.com>
2023-05-31 03:04:55 -0300

    chore(Colors): added new properties for object

---
+++ mobile/src/constants/colors.ts
@@ -0,0 +1,25 @@
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
+
+export default {
+  light: {
+    text: "#000",
+    background: "#fff",
+    onBackground: "#000",
+    tint: tintColorLight,
+    tabIconDefault: "#000",
+    tabIconSelected: tintColorLight,
+    primary: "#0da",
+    secundary: "#9086FF",
+  },
+  dark: {
+    text: "#fff",
+    background: "#000",
+    onBackground: "#fff",
+    tint: tintColorDark,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorDark,
+    primary: "#0da",
+    secundary: "#9086FF",
+  },
+};
