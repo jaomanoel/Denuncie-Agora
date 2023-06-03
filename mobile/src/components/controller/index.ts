326946d546930a4a1bd435fb2c60383702bc3561
jaomanoel <belezajovaniatb@gmail.com>
2023-06-03 17:28:31 -0300

    chore(Controller): Created imports for controllers

---
+++ mobile/src/components/controller/index.ts
@@ -0,0 +1,3 @@
+export { ControlledSelectInput } from "./ControlledSelectInput";
+export { ControlledTextInput } from "./ControlledTextInput";
+export { ControlledDateInput } from "./ControlledDateInput";
