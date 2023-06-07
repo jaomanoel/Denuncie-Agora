37f2e7cc02c03cc9ee217912f3f56b9e314b0400
jaomanoel <belezajovaniatb@gmail.com>
2023-06-07 05:18:35 -0300

    chore(ReportRepository): Implemented findByAbout

--- backend/src/main/java/com/denuncieagora/denuncie/domain/repositories/ReportRepository.java
+++ backend/src/main/java/com/denuncieagora/denuncie/domain/repositories/ReportRepository.java
@@ -1,11 +1,16 @@
 package com.denuncieagora.denuncie.domain.repositories;
 
 import com.denuncieagora.denuncie.domain.entities.Report;
+import com.denuncieagora.denuncie.domain.enums.HateCrimeTypeEnum;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ReportRepository extends JpaRepository<Report, UUID> {
+
+    List<Report> findByAbout(HateCrimeTypeEnum hateCrimeType, Pageable pageable);
 }
