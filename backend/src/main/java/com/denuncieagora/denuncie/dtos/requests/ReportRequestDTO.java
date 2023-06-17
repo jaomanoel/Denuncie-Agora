46b1db4b085951d73c3a316b6329734a76852956
jaomanoel <belezajovaniatb@gmail.com>
2023-06-17 17:12:20 -0300

    chore(ReportRequestDTO): Update from javax.validation NotNull to jakarta.validation NotNull

--- backend/src/main/java/com/denuncieagora/denuncie/dtos/requests/ReportRequestDTO.java
+++ backend/src/main/java/com/denuncieagora/denuncie/dtos/requests/ReportRequestDTO.java
@@ -5,7 +5,7 @@ import lombok.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
