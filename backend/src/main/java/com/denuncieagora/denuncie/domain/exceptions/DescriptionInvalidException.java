904613edd3ab4c95d844ffb7204e7f885d49916e
jaomanoel <belezajovaniatb@gmail.com>
2023-06-20 21:01:33 -0300

    chore(DescriptionInvalidException): Created custom exception to description.

--- backend/src/main/java/com/denuncieagora/denuncie/domain/exceptions/DescriptionInvalidException.java
+++ backend/src/main/java/com/denuncieagora/denuncie/domain/exceptions/DescriptionInvalidException.java
@@ -1,2 +1,8 @@
-package com.denuncieagora.denuncie.domain.exceptions;public class DescriptionInvalidException {
+package com.denuncieagora.denuncie.domain.exceptions;
+
+public class DescriptionInvalidException extends RuntimeException{
+
+    public DescriptionInvalidException(String message) {
+        super(message);
+    }
 }
