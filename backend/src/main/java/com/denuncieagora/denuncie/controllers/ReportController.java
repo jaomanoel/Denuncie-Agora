dfac9cd69deccfd3992396d794be0b4a04900a48
jaomanoel <belezajovaniatb@gmail.com>
2023-06-20 21:23:45 -0300

    chore(ReportController): Updated  from request param to path variable on method getById

--- backend/src/main/java/com/denuncieagora/denuncie/controllers/ReportController.java
+++ backend/src/main/java/com/denuncieagora/denuncie/controllers/ReportController.java
@@ -2,7 +2,6 @@ package com.denuncieagora.denuncie.controllers;
 
 import com.denuncieagora.denuncie.domain.enums.HateCrimeTypeEnum;
 import com.denuncieagora.denuncie.domain.services.ReportService;
-import com.denuncieagora.denuncie.dtos.requests.ReportDeleteRequestDTO;
 import com.denuncieagora.denuncie.dtos.requests.ReportRequestDTO;
 import com.denuncieagora.denuncie.dtos.responses.ReportResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +25,12 @@ public class ReportController {
         return service.getAll(hateCrime);
     }
 
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ReportResponseDTO getById(@PathVariable UUID id){
+        return service.getById(id);
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ReportResponseDTO create(@RequestBody @Valid ReportRequestDTO requestDTO)  {
@@ -42,11 +47,5 @@ public class ReportController {
     @ResponseStatus(HttpStatus.OK)
     private ReportResponseDTO edit(@RequestBody @Valid ReportRequestDTO request, @RequestParam(name = "id") UUID id){
         return service.edit(request, id);
-    }
-
-    @GetMapping("/")
-    @ResponseStatus(HttpStatus.OK)
-    public ReportResponseDTO getById(@RequestParam UUID id){
-        return service.getById(id);
     }
 }
