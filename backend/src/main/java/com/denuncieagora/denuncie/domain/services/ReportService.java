8b0aa1615e223371a7f85247c2b5cf2bc661586c
jaomanoel <belezajovaniatb@gmail.com>
2023-06-20 21:06:22 -0300

    chore(ReportService): validations created for the methods.

--- backend/src/main/java/com/denuncieagora/denuncie/domain/services/ReportService.java
+++ backend/src/main/java/com/denuncieagora/denuncie/domain/services/ReportService.java
@@ -2,17 +2,19 @@ package com.denuncieagora.denuncie.domain.services;
 
 import com.denuncieagora.denuncie.domain.entities.Report;
 import com.denuncieagora.denuncie.domain.enums.HateCrimeTypeEnum;
+import com.denuncieagora.denuncie.domain.exceptions.AboutInvalidException;
+import com.denuncieagora.denuncie.domain.exceptions.IdentityInvalidException;
+import com.denuncieagora.denuncie.domain.exceptions.ReportNotFoundException;
 import com.denuncieagora.denuncie.domain.repositories.ReportRepository;
 import com.denuncieagora.denuncie.dtos.requests.ReportRequestDTO;
 import com.denuncieagora.denuncie.dtos.responses.ReportResponseDTO;
-import com.denuncieagora.denuncie.mappers.ReportMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
@@ -26,14 +28,15 @@ public class ReportService {
     @Autowired
     private ValidateReportData validate;
 
-    @Autowired
-    private ReportMapper mapper;
-
     public List<ReportResponseDTO> getAll(HateCrimeTypeEnum hateCrime){
-        List<ReportResponseDTO> response = repository
-                .findAll(PageRequest.of(0, 5, Sort.by("date").descending()))
-                .stream().map(this::toResponse)
-                .toList();
+        List<ReportResponseDTO> response = new ArrayList<>();
+
+        if(hateCrime == null) {
+            response = repository
+                    .findAll(PageRequest.of(0, 5, Sort.by("date").descending()))
+                    .stream().map(this::toResponse)
+                    .toList();
+        }
 
         if(hateCrime != null){
             response = repository
@@ -45,43 +48,35 @@ public class ReportService {
         return response;
     }
 
-    public ReportResponseDTO getById(UUID uuid){
+    public ReportResponseDTO getById(UUID uuid) {
         Report report = repository.findById(uuid)
-                .orElseThrow(() -> new IllegalArgumentException("id is invalid!"));
+                .orElseThrow(() -> new ReportNotFoundException("id = " + uuid + " not found!"));
 
         return toResponse(report);
     }
 
     public ReportResponseDTO create(ReportRequestDTO request){
+        validate.validateIdentity(request.getIdentity());
+        validate.validateDate(request.getDate());;
+        validate.validateDescription(request.getDescription());
+        validate.validateCity(request.getCity());
+        validate.validateState(request.getState());
+
         Report report = toModel(request);
 
-        if(report.getDate().isAfter(LocalDate.now())) {
-            throw new IllegalArgumentException("Date is invalid!");
-        }
-
-        if(report.getIdentity().equals("")) {
-            throw new IllegalArgumentException("Report is invalid!");
-        }
-
-        if(report.getDescription().length() <= 200) {
-            throw new IllegalArgumentException("Description is invalid!");
-        }
-
-        repository.save(report);
+        report = repository.save(report);
 
         return toResponse(report);
     }
 
     @Transactional
     public void delete(UUID id, String identity){
-        if(identity.equals("")) {
-            throw new IllegalArgumentException("Identity is required!");
-        }
+        validate.validateIdentity(identity);
 
         Report model = repository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("id is invalid"));
+                .orElseThrow(() -> new ReportNotFoundException("Id = " + id + " is invalid."));
 
-        if(!model.getIdentity().equals(identity)) throw new IllegalArgumentException("Identity is invalid!");
+        if(!model.getIdentity().equals(identity)) throw new IdentityInvalidException("Identity is invalid!");
 
         repository.delete(model);
     }
@@ -91,9 +86,9 @@ public class ReportService {
         validate.validateId(id);
 
         Report model = repository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Report not found!"));
+                .orElseThrow(() -> new ReportNotFoundException("id = " + id + " not found!"));
 
-        if(!model.getIdentity().equals(request.getIdentity())) throw new IllegalArgumentException("Identity is invalid");
+        if(!model.getIdentity().equals(request.getIdentity())) throw new IdentityInvalidException("Identity is invalid!");
 
         validate.validateDate(request.getDate());
         validate.validateDescription(request.getDescription());
@@ -115,7 +110,7 @@ public class ReportService {
         return Stream.of(HateCrimeTypeEnum.values())
                 .filter(type -> type.getId().equals(value))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("About is invalid!"));
+                .orElseThrow(() -> new AboutInvalidException("This kind of hate crime is not valid."));
     }
 
     private Report toModel(ReportRequestDTO requestDTO) {
