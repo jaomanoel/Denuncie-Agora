38b8f33c28f2abd2cec2a33e2cccf50cda2fca90
jaomanoel <belezajovaniatb@gmail.com>
2023-06-20 21:00:44 -0300

    chore(CityInvalidException): Created custom exception for city.

--- backend/src/main/java/com/denuncieagora/denuncie/domain/exceptions/CityInvalidException.java
+++ backend/src/main/java/com/denuncieagora/denuncie/domain/exceptions/CityInvalidException.java
@@ -1,2 +1,8 @@
-package com.denuncieagora.denuncie.domain.exceptions;public class CityInvalidException {
+package com.denuncieagora.denuncie.domain.exceptions;
+
+public class CityInvalidException extends RuntimeException {
+
+    public CityInvalidException(String message) {
+        super(message);
+    }
 }
