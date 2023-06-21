cebf910f6c0c3d6c6bf6a1a8c3f98c7a7bdfcb6c
jaomanoel <belezajovaniatb@gmail.com>
2023-06-20 21:27:48 -0300

    test(ReportCreateControllerTest): Created test for method create on report controller

--- backend/src/test/java/com/denuncieagora/denuncie/controllers/ReportCreateControllerTest.java
+++ backend/src/test/java/com/denuncieagora/denuncie/controllers/ReportCreateControllerTest.java
@@ -1,12 +1,13 @@
 package com.denuncieagora.denuncie.controllers;
 
 import com.denuncieagora.denuncie.ApplicationConfigTest;
+import com.denuncieagora.denuncie.domain.exceptions.*;
 import com.denuncieagora.denuncie.domain.services.ReportService;
 import com.denuncieagora.denuncie.dtos.requests.ReportRequestDTO;
 import com.denuncieagora.denuncie.dtos.responses.ReportResponseDTO;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.coyote.http11.upgrade.UpgradeServletOutputStream;
+import jakarta.servlet.ServletException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -68,12 +69,18 @@ public class ReportCreateControllerTest extends ApplicationConfigTest {
         ReportRequestDTO requestDTO = new ReportRequestDTO();
         String requestBody = objectMapper.writeValueAsString(requestDTO);
 
-        Mockito.when(service.create(requestDTO)).thenThrow(new IllegalArgumentException("Report is invalid!"));
+        Mockito.when(service.create(requestDTO)).thenThrow(new IdentityInvalidException("Identity is invalid!"));
 
-        mockMvc.perform(post("/reports")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(requestBody))
-                .andExpect(status().isBadRequest());
+        Exception exception = Assertions.assertThrows(ServletException.class, () -> {
+            mockMvc.perform(post("/reports")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody))
+                    .andExpect(status().isBadRequest());
+        });
+
+        Throwable rootCause = exception.getCause();
+        Assertions.assertTrue(rootCause instanceof IdentityInvalidException);
+        Assertions.assertEquals("Identity is invalid!", rootCause.getMessage());
     }
 
     @Test
@@ -89,12 +96,18 @@ public class ReportCreateControllerTest extends ApplicationConfigTest {
 
         String requestBody = objectMapper.writeValueAsString(requestDTO);
 
-        Mockito.when(service.create(requestDTO)).thenThrow(new IllegalArgumentException("Description is invalid!"));
+        Mockito.when(service.create(requestDTO)).thenThrow(new DescriptionInvalidException("The description is short, the minimum is 200 characters"));
 
-        mockMvc.perform(post("/reports")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(status().isBadRequest());
+        Exception exception = Assertions.assertThrows(ServletException.class, () -> {
+            mockMvc.perform(post("/reports")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody))
+                    .andExpect(status().isBadRequest());
+        });
+
+        Throwable rootCause = exception.getCause();
+        Assertions.assertTrue(rootCause instanceof DescriptionInvalidException);
+        Assertions.assertEquals(rootCause.getMessage(), "The description is short, the minimum is 200 characters");
     }
 
     @Test
@@ -105,16 +118,103 @@ public class ReportCreateControllerTest extends ApplicationConfigTest {
         requestDTO.setState("Sao Paulo");
         requestDTO.setCity("Tubarao");
         requestDTO.setIdentity(UUID.randomUUID().toString());
-        requestDTO.setDescription("tete tete");
+        requestDTO.setDescription("test testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest testtest test");
         requestDTO.setDate(LocalDate.parse("2040-12-03"));
 
         String requestBody = objectMapper.writeValueAsString(requestDTO);
 
-        Mockito.when(service.create(requestDTO)).thenThrow(new IllegalArgumentException("Date is invalid!"));
+        Mockito.when(service.create(requestDTO)).thenThrow(new DateInvalidException("The date is invalid, the date cannot be greater than the current date."));
 
-        mockMvc.perform(post("/reports")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(status().isBadRequest());
+        Exception exception = Assertions.assertThrows(ServletException.class, () -> {
+            mockMvc.perform(post("/reports")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody))
+                    .andExpect(status().isBadRequest());
+        });
+
+        Throwable rootCause = exception.getCause();
+        Assertions.assertTrue(rootCause instanceof DateInvalidException);
+        Assertions.assertEquals(rootCause.getMessage(), "The date is invalid, the date cannot be greater than the current date.");
+    }
+
+    @Test
+    public void create_report_without_state_failure() throws Exception {
+
+        ReportRequestDTO requestDTO = new ReportRequestDTO();
+        requestDTO.setAbout(1);
+        requestDTO.setState("");
+        requestDTO.setCity("Tubarao");
+        requestDTO.setIdentity(UUID.randomUUID().toString());
+        requestDTO.setDescription("tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete ");
+        requestDTO.setDate(LocalDate.now());
+
+        String requestBody = objectMapper.writeValueAsString(requestDTO);
+
+        Mockito.when(service.create(requestDTO)).thenThrow(new StateInvalidException("State is invalid."));
+
+        Throwable exception = Assertions.assertThrows(ServletException.class, () -> {
+            mockMvc.perform(post("/reports")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody))
+                    .andExpect(status().isBadRequest());
+        });
+
+        Throwable rootCause = exception.getCause();
+        Assertions.assertTrue(rootCause instanceof StateInvalidException);
+        Assertions.assertEquals(rootCause.getMessage(), "State is invalid.");
+    }
+
+    @Test
+    public void create_report_without_city_failure() throws Exception {
+
+        ReportRequestDTO requestDTO = new ReportRequestDTO();
+        requestDTO.setAbout(1);
+        requestDTO.setState("Santa Catarina");
+        requestDTO.setCity("");
+        requestDTO.setIdentity(UUID.randomUUID().toString());
+        requestDTO.setDescription("tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete ");
+        requestDTO.setDate(LocalDate.now());
+
+        String requestBody = objectMapper.writeValueAsString(requestDTO);
+
+        Mockito.when(service.create(requestDTO)).thenThrow(new CityInvalidException("City is invalid."));
+
+        Exception exception = Assertions.assertThrows(ServletException.class, () ->  {
+            mockMvc.perform(post("/reports")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody))
+                    .andExpect(status().isBadRequest());
+        });
+
+        Throwable rootCause = exception.getCause();
+        Assertions.assertTrue(rootCause instanceof CityInvalidException);
+        Assertions.assertEquals(rootCause.getMessage(), "City is invalid.");
+    }
+
+    @Test
+    public void create_report_without_about_failure() throws Exception {
+
+        ReportRequestDTO requestDTO = new ReportRequestDTO();
+        requestDTO.setAbout(0);
+        requestDTO.setState("Santa Catarina");
+        requestDTO.setCity("Tubarao");
+        requestDTO.setIdentity(UUID.randomUUID().toString());
+        requestDTO.setDescription("tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete tete ");
+        requestDTO.setDate(LocalDate.now());
+
+        String requestBody = objectMapper.writeValueAsString(requestDTO);
+
+        Mockito.when(service.create(requestDTO)).thenThrow(new AboutInvalidException("This kind of hate crime is not valid."));
+
+        Exception exception = Assertions.assertThrows(ServletException.class, () -> {
+            mockMvc.perform(post("/reports")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody))
+                    .andExpect(status().isBadRequest());
+        });
+
+        Throwable rootCause = exception.getCause();
+        Assertions.assertTrue(rootCause instanceof AboutInvalidException);
+        Assertions.assertEquals(rootCause.getMessage(), "This kind of hate crime is not valid.");
     }
 }
