417d86f24319065338b082948046302554355c66
jaomanoel <belezajovaniatb@gmail.com>
2023-06-20 21:04:09 -0300

    chore(ValidateReportData): Created a class to handle validations

--- backend/src/main/java/com/denuncieagora/denuncie/domain/services/ValidateReportData.java
+++ backend/src/main/java/com/denuncieagora/denuncie/domain/services/ValidateReportData.java
@@ -1,9 +1,8 @@
 package com.denuncieagora.denuncie.domain.services;
 
-import com.denuncieagora.denuncie.domain.entities.Report;
+import com.denuncieagora.denuncie.domain.exceptions.*;
 import org.springframework.stereotype.Service;
 
-import java.rmi.server.UID;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -12,31 +11,37 @@ public class ValidateReportData {
 
     public void validateId(UUID id) {
         if(id.toString().equals("")) {
-            throw new IllegalArgumentException("Id is invalid!");
+            throw new ReportNotFoundException("Id is invalid!");
+        }
+    }
+
+    public void validateIdentity(String identity) {
+        if(identity.equals("")) {
+            throw new IdentityInvalidException("Identity is invalid!");
         }
     }
 
     public void validateDate(LocalDate date) {
         if (date == null || date.isAfter(LocalDate.now())) {
-            throw new IllegalArgumentException("Date is invalid!");
+            throw new DateInvalidException("The date is invalid, the date cannot be greater than the current date.");
         }
     }
 
     public void validateDescription(String description) {
         if (description.length() <= 200) {
-            throw new IllegalArgumentException("Description is invalid!");
+            throw new DescriptionInvalidException("The description is short, the minimum is 200 characters.");
         }
     }
 
     public void validateState(String state) {
         if (state.equals("")) {
-            throw new IllegalArgumentException("State is required!");
+            throw new StateInvalidException("State is invalid.");
         }
     }
 
     public void validateCity(String city) {
         if (city.equals("")) {
-            throw new IllegalArgumentException("City is required!");
+            throw new CityInvalidException("City is invalid.");
         }
     }
 }
