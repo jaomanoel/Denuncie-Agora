2ed57555801f3efb5a93147cb2d0e358bff8dfd2
jaomanoel <belezajovaniatb@gmail.com>
2023-06-22 01:01:45 -0300

    chore(CityResponseDTO): Created dto response for get city

---
+++ backend/src/main/java/com/denuncieagora/denuncie/dtos/responses/CityResponseDTO.java
@@ -0,0 +1,14 @@
+package com.denuncieagora.denuncie.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CityResponseDTO {
+
+    private Long id;
+    private String nome;
+}
