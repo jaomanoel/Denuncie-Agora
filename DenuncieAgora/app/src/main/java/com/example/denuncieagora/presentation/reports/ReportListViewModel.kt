2c662a0d864da1a7af71350b457efad9f2b88d09
jaomanoel <belezajovaniatb@gmail.com>
2023-06-24 18:10:20 -0300

    chore(ReportListViewModel): Removed function getReportId

--- DenuncieAgora/app/src/main/java/com/example/denuncieagora/presentation/reports/ReportListViewModel.kt
+++ DenuncieAgora/app/src/main/java/com/example/denuncieagora/presentation/reports/ReportListViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.denuncieagora.presentation.reports
 
 import android.content.Context
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.example.denuncieagora.domain.useCases.reports.GetReports
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -9,7 +8,6 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.viewModelScope
 import com.example.denuncieagora.domain.useCases.reports.DeleteReport
-import com.example.denuncieagora.domain.useCases.reports.GetReportById
 import com.example.denuncieagora.utils.Resource
 import com.example.denuncieagora.utils.getAdvertisingInfo
 import kotlinx.coroutines.flow.launchIn
@@ -21,7 +19,6 @@ import javax.inject.Inject
 class ReportListViewModel @Inject constructor(
     private val getReportsList: GetReports,
     private val deleteReport: DeleteReport,
-    private val getReportById: GetReportById
 ): ViewModel() {
     private val _state = mutableStateOf(ReportListState())
     val state: State<ReportListState> = _state
@@ -32,13 +29,13 @@ class ReportListViewModel @Inject constructor(
     }
 
     fun getReports(hateCrimeName: String? = null) {
-        if(hateCrimeName != null) {
-            _state.value = ReportListState(selectedReport = hateCrimeName)
-        }
         getReportsList(hateCrimeName).onEach { result ->
             when(result) {
                 is Resource.SuccessReports -> {
-                    _state.value = ReportListState(reports = result.data ?: emptyList())
+                    _state.value = ReportListState(
+                        reports = result.data ?: emptyList(),
+                        selectedReport = hateCrimeName ?: "Todos"
+                    )
                 }
                 is Resource.Error -> {
                     _state.value = ReportListState(error = result.message ?: "An unexpected error occured")
@@ -68,31 +65,7 @@ class ReportListViewModel @Inject constructor(
         }.launchIn(viewModelScope)
     }
 
-   /* fun getReportId(id: UUID) {
-
-        getReportById(id).onEach { result ->
-            when(result) {
-                is Resource.SuccessReport -> {
-                    if(result.data?.identity != stateIdentity.value) {
-                        getReports()
-                    }
-
-                    if(result.data?.identity == stateIdentity.value) {
-                        deleteReportClick(result.data.id, stateIdentity.value)
-                    }
-                }
-                is Resource.Error -> {
-                    _state.value = ReportListState(error = result.message ?: "An unexpected error occured")
-                }
-                is Resource.Loading -> {
-                    _state.value = ReportListState(isLoading = true)
-                }
-                else -> {}
-            }
-        }.launchIn(viewModelScope)
-    }*/
-
-    fun getIdentity(context: Context): Unit {
+    fun getIdentity(context: Context) {
         getAdvertisingInfo(context) { advertisingId ->
             stateIdentity.value = advertisingId
         }
